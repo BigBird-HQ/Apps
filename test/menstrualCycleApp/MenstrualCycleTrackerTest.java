1c10fcb5480a992ad4d6cc49e033887e376c22e8
MeshackYaro <yaromeshack@gmail.com>
2024-02-08 06:35:09 +0100

    update

--- test/menstrualCycleApp/MenstrualCycleTrackerTest.java
+++ test/menstrualCycleApp/MenstrualCycleTrackerTest.java
@@ -1,6 +1,5 @@
-package menstrualApp;
+package menstrualCycleApp;
 
-import menstrualCycleApp.MenstrualCycleTracker;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
